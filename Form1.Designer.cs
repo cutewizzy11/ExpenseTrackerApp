a0be9a8d57eba2ee0ce01a69d19fbd00db2516f1
Paul Anyebe <p.anyebe9@gmail.com>
2024-07-25 12:49:48 +0100

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,21 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelAmount;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.ListBox listBoxExpenses;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelTotalAmount;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,19 +28,181 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.listBoxExpenses = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelTotalAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(9, 12);
+            this.labelDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(63, 13);
+            this.labelDescription.TabIndex = 0;
+            this.labelDescription.Text = "Description:";
+            // 
+            // labelAmount
+            // 
+            this.labelAmount.AutoSize = true;
+            this.labelAmount.Location = new System.Drawing.Point(9, 35);
+            this.labelAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(46, 13);
+            this.labelAmount.TabIndex = 1;
+            this.labelAmount.Text = "Amount:";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(9, 58);
+            this.labelDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(33, 13);
+            this.labelDate.TabIndex = 2;
+            this.labelDate.Text = "Date:";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(76, 10);
+            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(204, 20);
+            this.textBoxDescription.TabIndex = 3;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(76, 32);
+            this.textBoxAmount.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(204, 20);
+            this.textBoxAmount.TabIndex = 4;
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.Location = new System.Drawing.Point(76, 55);
+            this.dateTimePickerDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(204, 20);
+            this.dateTimePickerDate.TabIndex = 5;
+            // 
+            // listBoxExpenses
+            // 
+            this.listBoxExpenses.FormattingEnabled = true;
+            this.listBoxExpenses.Location = new System.Drawing.Point(11, 78);
+            this.listBoxExpenses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxExpenses.Name = "listBoxExpenses";
+            this.listBoxExpenses.Size = new System.Drawing.Size(269, 160);
+            this.listBoxExpenses.TabIndex = 6;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(298, 9);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(70, 19);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(298, 32);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(70, 19);
+            this.buttonEdit.TabIndex = 8;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(298, 55);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(70, 19);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(298, 78);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(70, 19);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(9, 248);
+            this.labelTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(34, 13);
+            this.labelTotal.TabIndex = 11;
+            this.labelTotal.Text = "Total:";
+            // 
+            // labelTotalAmount
+            // 
+            this.labelTotalAmount.AutoSize = true;
+            this.labelTotalAmount.Location = new System.Drawing.Point(76, 248);
+            this.labelTotalAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTotalAmount.Name = "labelTotalAmount";
+            this.labelTotalAmount.Size = new System.Drawing.Size(13, 13);
+            this.labelTotalAmount.TabIndex = 12;
+            this.labelTotalAmount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(378, 270);
+            this.Controls.Add(this.labelTotalAmount);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxExpenses);
+            this.Controls.Add(this.dateTimePickerDate);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelAmount);
+            this.Controls.Add(this.labelDescription);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Expense Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ExpenseTrackerApp
 {
     public partial class Form1 : Form
     {
+        private const string FilePath = "expenses.txt";
+        private List<Expense> expenses = new List<Expense>();
+
         public Form1()
         {
             InitializeComponent();
+            LoadExpenses();
+            UpdateTotalAmount();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string description = textBoxDescription.Text.Trim();
+            if (decimal.TryParse(textBoxAmount.Text.Trim(), out decimal amount) && amount > 0)
+            {
+                DateTime date = dateTimePickerDate.Value;
+
+                Expense newExpense = new Expense(description, amount, date);
+                expenses.Add(newExpense);
+                listBoxExpenses.Items.Add(newExpense);
+                ClearInputFields();
+                UpdateTotalAmount();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid amount.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (listBoxExpenses.SelectedIndex >= 0)
+            {
+                Expense selectedExpense = (Expense)listBoxExpenses.SelectedItem;
+                selectedExpense.Description = textBoxDescription.Text.Trim();
+                if (decimal.TryParse(textBoxAmount.Text.Trim(), out decimal amount) && amount > 0)
+                {
+                    selectedExpense.Amount = amount;
+                    selectedExpense.Date = dateTimePickerDate.Value;
+
+                    listBoxExpenses.Items[listBoxExpenses.SelectedIndex] = selectedExpense;
+                    ClearInputFields();
+                    UpdateTotalAmount();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid amount.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an expense to edit.", "Edit Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxExpenses.SelectedIndex >= 0)
+            {
+                expenses.RemoveAt(listBoxExpenses.SelectedIndex);
+                listBoxExpenses.Items.RemoveAt(listBoxExpenses.SelectedIndex);
+                ClearInputFields();
+                UpdateTotalAmount();
+            }
+            else
+            {
+                MessageBox.Show("Please select an expense to delete.", "Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveExpenses();
+            MessageBox.Show("Expenses saved successfully.", "Save Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void LoadExpenses()
+        {
+            if (File.Exists(FilePath))
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('|');
+                    if (parts.Length == 3 && decimal.TryParse(parts[1], out decimal amount))
+                    {
+                        Expense expense = new Expense(parts[0], amount, DateTime.Parse(parts[2]));
+                        expenses.Add(expense);
+                        listBoxExpenses.Items.Add(expense);
+                    }
+                }
+            }
+        }
+
+        private void SaveExpenses()
+        {
+            List<string> lines = expenses.Select(e => $"{e.Description}|{e.Amount}|{e.Date}").ToList();
+            File.WriteAllLines(FilePath, lines);
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxDescription.Clear();
+            textBoxAmount.Clear();
+            dateTimePickerDate.Value = DateTime.Today;
+        }
+
+        private void UpdateTotalAmount()
+        {
+            decimal totalAmount = expenses.Sum(e => e.Amount);
+            labelTotalAmount.Text = totalAmount.ToString("C");
+        }
+    }
+
+    public class Expense
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Expense(string description, decimal amount, DateTime date)
+        {
+            Description = description;
+            Amount = amount;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{Description} - {Amount:C} - {Date:d}";
         }
     }
 }
